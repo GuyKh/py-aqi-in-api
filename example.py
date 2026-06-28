ad2e98958e7d4e8d56cd10b05deb89e5c35ff1c9
Guy Khmelnitsky <guykhmel@gmail.com>
2026-06-28 20:21:15 +0300

    Add example.py replicating examples/basic.ts

--- example.py
+++ example.py
@@ -1,40 +1,38 @@
 import asyncio
 
-from aqi_in_api import AQIClient
+from aqi_in_api import create_aqi_client
 
 
 async def main() -> None:
-    client = AQIClient()
+    client = create_aqi_client()
 
     ip_details = await client.get_ip_details()
-    print("IP Details:", ip_details)
+    print(ip_details)
 
     nearest_location = await client.get_nearest_location(
         lat=ip_details.lat,
         long=ip_details.lon,
     )
-    print("Nearest location:", nearest_location)
+    print(nearest_location)
 
     station = nearest_location[0].location_slug
 
     location_details = await client.get_location_by_slug(slug=station)
-    print("Location details:", location_details)
+    print(location_details)
 
     history = await client.get_last_24_hour_history(
         slug=station,
         sensorname="pm25",
         slug_type="locationId",
     )
-    print("24h history:", history)
+    print(history)
 
     history_30_days = await client.get_last_30_days_history(
         slug=station,
         sensorname="pm25",
         slug_type="locationId",
     )
-    print("30 days history:", history_30_days)
-
-    await client.close()
+    print(history_30_days)
 
 
 asyncio.run(main())
